3397013e83d19b7b7038afdc951e4474742dcb12
Daniel Mora C. <moraisurcan@hotmail.com>
2026-03-19 22:48:01 -0500

    Feat: Listar workpackages endpoint added

---
+++ Web/Infrastructure/Config/Extensions/ServicesExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Application.Services.UseCases.WorkPackages;
+using Web.Infrastructure.Config.Settings;
+using Web.Infrastructure.Services.UseCases.WorkPackages;
+
+namespace Web.Infrastructure.Config.Extensions;
+public static class ServicesExtensions
+{
+    public static IServiceCollection AddServices(this IServiceCollection collection, IConfiguration configuration)
+    {
+        collection.AddKeyedSingleton<IApiSettings, OpenProjectSettings>(nameof(KeyService.OpenProjectSettings));
+        collection.AddScoped<IListsWorkPackagesCommand, ListsWorkPackagesCommandImpl>();
+        return collection;
+    }
+
+}
---
+++ Web/Infrastructure/Config/Settings/IApiSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Web.Infrastructure.Config.Settings;
+
+public interface IApiSettings
+{
+    string GetHttpClientName();
+    string GetUri();
+}
