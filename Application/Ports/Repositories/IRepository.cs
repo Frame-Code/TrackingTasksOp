8ef9dfe7ddd04d57f05def2e01ca5091161c0217
Daniel Mora C. <moraisurcan@hotmail.com>
2026-03-20 22:42:39 -0500

    feat: repositorios added

---
+++ Application/Ports/Repositories/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq.Expressions;
+
+namespace Application.Ports.Repositories;
+
+public interface IRepository <T>
+{
+    Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>>? filter, bool tracking = false);
+    Task<T?> GetByIdAsync(int id, bool tracking = false);
+    Task<T> SaveAsync(T task);
+}
---
+++ Web/Infrastructure/Adapters/Repositories/TaskRepositoryImpl.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq.Expressions;
+using Application.Ports.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Web.Infrastructure.DataAccess;
+using Task = Domain.Entities.TrackingTasksEntities.Task;
+
+namespace Web.Infrastructure.Adapters.Repositories;
+
+public class TaskRepositoryImpl(TrackingTasksDbContext context) : ITaskRepository
+{
+    public async Task<IEnumerable<Task>> GetAllAsync(Expression<Func<Task, bool>>? filter, bool tracking = false)
+    {
+        var query = tracking ? context.Tasks.AsQueryable() : context.Tasks.AsNoTracking().AsQueryable();
+        return filter is null? await query.ToListAsync() 
+            : await query.Where(filter).ToListAsync();
+    }
+
+    public async Task<Task?> GetByIdAsync(int id, bool tracking = false)
+    {
+        var query = tracking? context.Tasks.AsQueryable() : context.Tasks.AsNoTracking().AsQueryable();
+        return await query.FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task<Task> SaveAsync(Task task)
+    {
+        var taskSaved = await context.Tasks.AddAsync(task);
+        await context.SaveChangesAsync();
+        return taskSaved.Entity;
+    }
+
+    public async Task<Task?> GetByOpenProjectIdAsync(int id, bool tracking = false)
+    {
+        var query = tracking? context.Tasks.AsQueryable() : context.Tasks.AsNoTracking().AsQueryable();
+        return await query.FirstOrDefaultAsync(x => x.OpenProjectId == id);
+    }
+}
